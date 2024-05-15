a620fb71c2cbcd8caf95ca7e6b65091b98b97ca6
xian web_dev / <163428944+jaucianweb@users.noreply.github.com>
2024-05-15 12:37:49 +0800

    Add files via upload

---
+++ Pomodoro Timer/script.js
@@ -0,0 +1,43 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+const timer = document.getElementById('timer');
+
+
+let interval;
+
+let timeLeft = 1499;
+
+function update(){
+    let minutes = Math.floor(timeLeft / 60);
+    let seconds = timeLeft % 60 ;
+    let formattedTime =` ${minutes} :  ${seconds}`;
+
+    timer.innerHTML = formattedTime;
+}
+
+function startTimer(){
+    interval = setInterval(() => {
+        timeLeft--;
+        update();
+        if(timeLeft === 0){
+            alert('Times Up');
+            timeLeft = 1499;
+        }
+    }, 1000);
+}
+    
+
+function stopTimer(){
+    clearInterval(interval);
+}
+function resetTimer(){
+    clearInterval(interval);
+    timeLeft = 1499;
+    update();
+    
+}
+
+start.addEventListener('click', startTimer);
+stop.addEventListener('click', stopTimer);
+reset.addEventListener('click', resetTimer);
